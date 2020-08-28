2d8859115b1edf896b2aa489e235451b801892c1
Camryn Patterson <camrynpatterson@yahoo.com>
2020-08-28 05:59:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   current_key_assoc_w_min_val = nil 
   name_hash.each do |key, value|
 
-  if current_minimum_val == nil 
+if current_minimum_val == nil 
       current_minimum_val = value 
       current_key_assoc_w_min_val = key 
     else 
